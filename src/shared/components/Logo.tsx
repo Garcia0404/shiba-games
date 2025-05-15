03c9e716a2d073fd8178c46aaf0454b1cf07219e
Garcia0404 <julioxenrique@gmail.com>
2025-05-14 21:46:01 -0500

    vercel.json

--- src/shared/components/Logo.tsx
+++ src/shared/components/Logo.tsx
@@ -3,12 +3,13 @@ export const Logo = () => {
   return (
     <Link to="/" className="flex items-center">
       <img
-        src="https://zeroxgames.gg/cdn/shop/files/color.png?v=1743045115&width=600"
-        alt="ZeroGames Logo"
+        src="/logo.png"
+        alt="Shiba Games logo"
         width={150}
         height={30}
-        className="h-8 w-auto"
+        className="h-12 w-auto"
       />
+      <span className="text-xl font-extrabold text-[#ff00009d]">SHIBA</span>
     </Link>
   )
 }
